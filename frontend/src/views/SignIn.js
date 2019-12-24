e01946fcdf2b00d33f540a8ed01a35fd909e67d0
Qwizi <qwizi95@protonmail.com>
2019-12-24 12:47:37 +0100

    Added react app, created shop view

---
+++ frontend/src/views/SignIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class SignIn extends React.Component
+{
+    render() {
+        return (
+            <div>
+                Sign-In
+            </div>
+        );
+    }
+}
--- shark_core/api/urls.py
+++ shark_core/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 app_name = 'api'
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_optain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('store/', include('store.api_urls'))
 ]
