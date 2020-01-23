dc8daa0415ac666d1b7a3d573ab326e36c2a8d70
Adrian <qwizi95@protonmail.com>
2020-01-23 17:11:02 +0100

    CCos

--- backend/shark_core/asgi.py
+++ backend/shark_core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for shark_core project.
+ASGI config for shark_coree project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shark_core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shark_coree.settings')
 
 application = get_asgi_application()
--- backend/shark_core/urls.py
+++ backend/shark_core/urls.py
@@ -1,4 +1,4 @@
-"""shark_core URL Configuration
+"""shark_coree URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
