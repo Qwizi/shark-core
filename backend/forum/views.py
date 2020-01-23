41e422729cd4799c568761f2947340b739231e6f
Adrian <qwizi95@protonmail.com>
2020-01-23 17:10:40 +0100

    Changed localization files

--- backend/forum/views.py
+++ backend/forum/views.py
@@ -2,7 +2,6 @@ from rest_framework import viewsets, status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from accounts.models import Account
 from .models import (
     Category,
     Thread,
