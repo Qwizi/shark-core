dd33126d68608201b631562e9fb623872310833b
Adrian <qwizi95@protonmail.com>
2020-01-29 17:17:17 +0100

    Changed accounts views and add swagger

--- backend/accounts/tests/models.py
+++ backend/accounts/tests/models.py
@@ -1,9 +1,2 @@
 from django.test import TestCase
 from ..models import Account
-
-class AccountModelTestCase(TestCase):
-
-    def setUp(self):
-        Account.objects.create(
-            ""
-        )
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import AccountView, AccountAuthSteamTokenView
+from .views import (
+    account_list,
+    account_me,
+    AccountView,
+)
 
 router = routers.DefaultRouter()
 router.register(r'', AccountView, basename='accounts')
@@ -9,5 +13,7 @@ router.register(r'', AccountView, basename='accounts')
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', account_list, name='accounts-list'),
+    path('me/', account_me, name='accounts-me')
+    #path('', include(router.urls)),
 ]
