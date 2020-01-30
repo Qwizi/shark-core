24c3d0ef90a5ad1cbec7bbde44a4aef82534b6b6
Adrian <qwizi95@protonmail.com>
2020-01-30 15:01:16 +0100

    Added default groups when migrating

--- backend/forum/permissions.py
+++ backend/forum/permissions.py
@@ -1,9 +1,41 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsAuthenticatedOnCreateUpdate(BasePermission):
+class IsAdminOnCreateUpdateDelete(BasePermission):
     def has_permission(self, request, view):
-        if request.method in ['POST', 'PUT']:
+        if request.method in ['POST', 'PUT', 'DELETE']:
+            return bool(request.user and request.user.is_staff)
+        else:
+            return True
+
+
+class IsAuthenticatedOnCreate(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['POST']:
+            return bool(request.user and request.user.is_authenticated)
+        else:
+            return True
+
+
+class IsAuthenticatedOnUpdate(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['PUT']:
+            return bool(request.user and request.user.is_authenticated)
+        else:
+            return True
+
+
+class IsAuthenticatedOnDelete(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['DELETE']:
+            return bool(request.user and request.user.is_authenticated)
+        else:
+            return True
+
+
+class IsAuthenticatedOnCreateUpdateDelete(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['POST', 'PUT', 'DELETE']:
             return bool(request.user and request.user.is_authenticated)
         else:
             return True
--- backend/forum/urls.py
+++ backend/forum/urls.py
@@ -1,16 +1,22 @@
+from django.urls import path
 from rest_framework import routers
 
 from .views import (
-    ForumCategoryViewSet,
+    forum_category_list,
+    forum_category_detail,
+    #forum_category_create,
     ForumThreadViewSet,
     ForumPostViewSet,
     ForumCommentViewSet
 )
 
 router = routers.DefaultRouter()
-router.register(r'categories', ForumCategoryViewSet)
 router.register(r'threads', ForumThreadViewSet, basename='threads')
 router.register(r'posts', ForumPostViewSet, basename='posts')
 router.register(r'comments', ForumCommentViewSet, basename='comments')
 
-urlpatterns = router.urls
+urlpatterns = [
+    #path('categories/', forum_category_create, name='forum-category-create'),
+    path('categories/', forum_category_list, name='forum-category-list'),
+    path('categories/<int:pk>/', forum_category_detail, name='forum-category-detail'),
+]
