ff5bc4dbe24228251e5781f3aa5311a2ddaa3dbb
Adrian Ciołek <500adrian2@wp.pl>
2020-04-25 12:35:31 +0200

    Added slug field in news model' (#19)

--- backend/news/models.py
+++ backend/news/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.conf import settings
+from django.template.defaultfilters import slugify
 
 
 class News(models.Model):
     title = models.CharField(max_length=80)
+    slug = models.SlugField(null=True)
     content = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
@@ -12,3 +14,8 @@ class News(models.Model):
 
     def __str__(self):
         return f'{self.title}  | {self.author.username}'
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super(News, self).save(*args, **kwargs)
